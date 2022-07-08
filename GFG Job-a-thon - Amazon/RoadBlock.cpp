88f7ec28aec09162cf5fa2192a4227c26565348c
Sagar Narsingani <sagarhn2411@gmail.com>
2022-07-08 09:48:03 +0530

    GFG Contest Codes

--- GFG Job-a-thon - Amazon/RoadBlock.cpp
+++ GFG Job-a-thon - Amazon/RoadBlock.cpp
@@ -36,40 +36,61 @@ vector<int> ResolveQueries(int N, int Q, vector<vector<int>> Edge, vector<vector
 
     getEulerTour(0, 0);
 
-    vector<int> prefix, suffix;
-    suffix.push_back(height[0]);
+    vector<int> prefix, suffix(2*N, height[0]);
+    suffix[2*N-1] = height[0];
     prefix.push_back(height[0]);
     for(int i=1; i<(2*N); i++){
         prefix.push_back(max(prefix[i-1], height[eulerTour[i]]));
-        suffix.push_back(max(suffix[i-1], height[eulerTour[(2*N-1)-i]]));
+        suffix[2*N-1-i] = max(suffix[2*N-i], height[eulerTour[(2*N-1)-i]]);
     }
 
     vector<int> ans;
     for(auto q: query){
-        int s = startIndex[q[1]] - 1;
-        int e =(2*N-1) - (endIndex[q[1]] + 1);
-        if(s<0 and e<0)
-            ans.push_back(0);
-        else if(s<0)
-            ans.push_back(suffix[e]);
-        else if(e<0)
-            ans.push_back(prefix[s]);
+        int s1 = startIndex[q[0]] - 1;
+        int e1 = endIndex[q[0]] + 1;
+        int s2 = startIndex[q[1]] - 1;
+        int e2 = endIndex[q[1]] + 1;
+        
+        int opt1, opt2;
+        if(s1<0 and e1>=2*N)
+            opt1 = 0;
+        else if(s1<0)
+            opt1 = suffix[e1];
+        else if(e1>=2*N)
+            opt1 = prefix[s1];
         else
-            ans.push_back(max(suffix[e], prefix[s]));
+            opt1 = max(prefix[s1], suffix[e1]);
+
+        if(s2<0 and e2>=2*N)
+            opt2 = 0;
+        else if(s2<0)
+            opt2 = suffix[e2];
+        else if(e2>=2*N)
+            opt2 = prefix[s2];
+        else
+            opt2 = max(prefix[s2], suffix[e2]);
+        
+        ans.push_back(max(opt1, opt2));
     }
 
     return ans;
 }
 
 int main(){
-    int N=3;
+    int N=9;
     vector<vector<int>> edges = {
-        {0, 1},
-        {0, 2}
+        {5, 7},
+        {0, 2},
+        {7, 8},
+        {0, 8},
+        {1, 2},
+        {2, 6},
+        {4, 8},
+        {0, 3}
     };
 
     vector<vector<int>> query = {
-        {0, 1}
+        {4, 8}
     };
 
     vector<int> ans = ResolveQueries(N, query.size(), edges, query);
